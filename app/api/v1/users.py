49121110ecf4e900d6d2b1be308de0f6cd9d6331
Sagar Bhoge <sagarbhoge2003@gmail.com>
2025-11-04 08:57:52 +0530

    sigin feat

--- app/api/v1/users.py
+++ app/api/v1/users.py
@@ -4,12 +4,25 @@ from app.models.user import UserSignUp, UserResponse, UserLogin, Token
 from app.core.dependencies import get_user_service,get_user_repository
 from app.services.user_service import UserService
 
+
 router = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/signup", response_model=UserResponse)
-async def signup(user: UserSignUp, service: UserService = Depends(get_user_service)):
-    return await service.signup_user(user)
+@router.post("/signup")
+async def signup():
+    return {"message": "signup"}
 
-@router.post("/login", response_model=Token)
-async def login(user: UserLogin, service: UserService = Depends(get_user_service)):
-    return await service.login_user(user)
+@router.post("/login")
+async def login():
+    return {"message": "login"}
+
+# Define a custom method
+async def test():
+    return {"message": "This is a Test"}
+
+# Register the route manually with custom method
+router.add_api_route(
+    "/me",
+    test,
+    methods=["SAGAR"],  # custom HTTP method
+    response_model=dict
+)
